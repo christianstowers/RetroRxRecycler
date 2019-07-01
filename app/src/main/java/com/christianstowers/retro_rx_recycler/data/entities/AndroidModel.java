23e089d2e725d8b4139cefb38afabf5dcd0f6e28
Christian Stowers <christian@paulconsulting.com>
2019-06-30 21:10:24 -0400

    some of the basic layout and model structure added. jumping on a branch

---
+++ app/src/main/java/com/christianstowers/retro_rx_recycler/data/entities/AndroidModel.java
@@ -0,0 +1,13 @@
+package com.christianstowers.retro_rx_recycler.data.entities;
+
+public class AndroidModel {
+
+    private String ver;
+    private String name;
+    private String api;
+
+    public String getVer() { return ver; }
+    public String getName() { return name; }
+    public String getApi() { return api; }
+
+}
